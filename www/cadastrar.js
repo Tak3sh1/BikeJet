3b653855723467e735bf604e60a7d958e633e1f1
Tak3sh1 <professionaltakeshi@gmail.com>
2023-12-15 03:48:51 +0900

    Project's Initial Commit

---
+++ www/cadastrar.js
@@ -0,0 +1,4 @@
+document.getElementById("log").addEventListener("click", function(event){
+ event.preventDefault(); // impede o comportamento padrão do botão (enviar o formulário)
+ window.location.href = "nova_pagina.html"; // redireciona para a nova página
+});
